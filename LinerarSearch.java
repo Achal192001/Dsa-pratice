7795caf22d56e8bf233eb5bc437103b2f2227eef
Achal192001 <achalchittalwar@gmail.com>
2025-12-02 17:36:39 +0530

    Add files via upload

---
+++ LinerarSearch.java
@@ -0,0 +1,20 @@
+public class LinerarSearch {
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 7, 1, 9, 3};
+        int target = 7;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
